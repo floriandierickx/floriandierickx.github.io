8fdb572d6833273f1a3e85c38bbf5aa6de060f15
Florian <floriandierickx@gmail.com>
2019-06-12 21:56:02 +0200

    load altmetric in bib-list.js to avoid non-synchronous loading

--- build/bib-list.js
+++ build/bib-list.js
@@ -2900,6 +2900,8 @@ var bibtexify = (function($) {
         $(chartIdSelector).html(statsHtml).after(legendHtml);
     };
 
+    !function(e,t,d){var n="createElement",c="getElementsByTagName",m="setAttribute",d=document.getElementById(e);return d&&d.parentNode&&d.parentNode.removeChild(d),d=document[n+"NS"]&&document.documentElement.namespaceURI,d=d?document[n+"NS"](d,"script"):document[n]("script"),d[m]("id",e),d[m]("src",t),(document[c]("head")[0]||document[c]("body")[0]).appendChild(d),d=new Image,void d[m]("src","https://d1uo4w7k31k5mn.cloudfront.net/donut/0.png")}("altmetric-embed-js","https://d1bxh8uas1mnw7.cloudfront.net/assets/altmetric_badges-71f30fc33cff2fd0a5de8e407f36bda8.js");
+
     // Creates a new publication list to the HTML element with ID
     // bibElemId. The bibsrc can be
     //   - a jQuery selector, in which case html of the element is used
